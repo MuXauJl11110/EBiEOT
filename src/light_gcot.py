Fix computation of potentials. Functions converge.

src/light_gcot.py
@@ -117,16 +117,16 @@ class LightGCOT(nn.Module):
     ) -> torch.Tensor:
         if self.A_diagonal_init is not None and self.B_diagonal_init is not None:
             diff = (
-                batched_x[None, None, :, :] - g_x[:, :, None, :]
-            )  # [1 x 1 x bs x x_dim] + [N x M x 1 x x_dim] = [N x M x bs x x_dim]
-            return -0.5 / self.epsilon * torch.sum(diff * G_xx[:, :, None, :] * diff, dim=(2, 3))  # [N x M]
+                batched_x[:, None, None, :] - g_x[None, :, :, :]
+            )  # [bs x 1 x 1 x x_dim] + [1 x N x M x x_dim] = [bs x N x M x x_dim]
+            return -0.5 / self.epsilon * torch.sum(diff * G_xx[None, :, :, :] * diff, dim=(3))  # [bs x N x M]
 
     def compute_primal_potential(self, batched_y: torch.Tensor) -> float:
-        diff = batched_y[None, :, :] - self.a_y[:, None, :]  # [1 x bs x y_dim] - [N x 1 x y_dim] = [N x bs x y_dim]
+        diff = batched_y[:, None, :] - self.a_y[None, :, :]  # [bs x 1 x y_dim] - [1 x N x y_dim] = [bs x N x y_dim]
         log_quadratic = (
-            -0.5 / self.epsilon * torch.sum(diff * self.A_yy_diagonal_matrix[:, None, :] * diff, dim=(1, 2))
-        )
-        return self.epsilon * torch.log(torch.sum(torch.exp(self.log_w + log_quadratic)))
+            -0.5 / self.epsilon * torch.sum(diff * self.A_yy_diagonal_matrix[None, :, :] * diff, dim=2)
+        )  # [bs x N]
+        return self.epsilon * torch.log(torch.sum(torch.exp(self.log_w[None, :] + log_quadratic), dim=1))  # [bs]
 
     def compute_dual_potential(self, batched_x: torch.Tensor) -> float:
         G_xx, G_xy, G_yx, G_yy = self.compute_G()
@@ -134,7 +134,7 @@ class LightGCOT(nn.Module):
         log_alpha = self.compute_log_alpha(G_xx, G_xy, G_yx, G_yy)
         log_quadratic = self.compute_log_quadratic(batched_x, g_x, G_xx, G_xy, G_yx, G_yy)
 
-        return -self.epsilon * torch.log(torch.sum(torch.exp(log_alpha + log_quadratic)))
+        return -self.epsilon * torch.log(torch.sum(torch.exp(log_alpha + log_quadratic), dim=(1, 2)))
 
     def set_epsilon(self, new_epsilon):
         self.epsilon = torch.tensor(new_epsilon, device=self.epsilon.device)

tests/test_potentials.py
@@ -8,11 +8,11 @@ def test_compute_log_quadratic(D: LightGCOT, batch_size: int):
     G_xx, G_xy, G_yx, G_yy = D.compute_G()
     g_x, _ = D.compute_g()
 
-    _log_quadratic = torch.zeros((D.n_potentials, D.m_potentials))
+    _log_quadratic = torch.zeros((batch_size, D.n_potentials, D.m_potentials))
     for n in range(D.n_potentials):
         for m in range(D.m_potentials):
             diff = x - g_x[n][m]
-            _log_quadratic[n][m] = -0.5 / D.epsilon * torch.sum(diff * G_xx[n][m] * diff)
+            _log_quadratic[:, n, m] = -0.5 / D.epsilon * torch.sum(diff * G_xx[n][m] * diff, dim=1)
 
     log_quadratic = D.compute_log_quadratic(x, g_x, G_xx, G_xy, G_yx, G_yy)
     assert torch.allclose(log_quadratic, _log_quadratic)
@@ -21,14 +21,14 @@ def test_compute_log_quadratic(D: LightGCOT, batch_size: int):
 def test_compute_primal_potential(D: LightGCOT, batch_size: int):
     y = torch.randn((batch_size, D.y_dim))
 
-    se = 0  # sum of exponents
+    se = torch.zeros((batch_size, D.n_potentials))  # sum of exponents
     for n in range(D.n_potentials):
         diff = y - D.a_y[n]  # [bs x y_dim] - [y_dim] = [bs x y_dim]
-        se += torch.exp(D.log_w[n]) * torch.exp(
-            (-0.5 / D.epsilon) * torch.sum(diff * D.A_yy_diagonal_matrix[n] * diff)
+        se[:, n] += torch.exp(D.log_w[n]) * torch.exp(
+            (-0.5 / D.epsilon) * torch.sum(diff * D.A_yy_diagonal_matrix[n] * diff, dim=1)
         )
 
-    _f = D.epsilon * torch.log(se)
+    _f = D.epsilon * torch.log(torch.sum(se, dim=1))
     f = D.compute_primal_potential(y)
     assert torch.allclose(f, _f)
 
@@ -39,19 +39,18 @@ def test_compute_dual_potential(D: LightGCOT, batch_size: int):
     g_x, _ = D.compute_g()
     c = D.compute_c()
 
-    se = torch.zeros((D.n_potentials, D.m_potentials))
+    se = torch.zeros((batch_size, D.n_potentials, D.m_potentials))
     for n in range(D.n_potentials):
         for m in range(D.m_potentials):
             diff = x - g_x[n][m]  # [bs x y_dim] - [y_dim] = [bs x y_dim]
-            se[n][m] += torch.exp(
+            se[:, n, m] += torch.exp(
                 D.log_w[n]
                 + D.log_v[m]
                 + 0.5 * (torch.log(2 * torch.pi * D.epsilon) * D.y_dim - torch.sum(torch.log(G_yy[n][m])))
                 - 0.5 / D.epsilon * c[n][m]
-                - 0.5 / D.epsilon * torch.sum(diff * G_xx[n][m] * diff)
+                - 0.5 / D.epsilon * torch.sum(diff * G_xx[n][m] * diff, dim=1)
             )
 
-    print(se[0][0])
-    _f_c = -D.epsilon * torch.log(torch.sum(se))
+    _f_c = -D.epsilon * torch.log(torch.sum(se, dim=(1, 2)))
     f_c = D.compute_dual_potential(x)
     assert torch.allclose(f_c, _f_c)