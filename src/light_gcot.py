Fixed computation. Method converged. TODO: change cost

src/light_gcot.py
@@ -43,7 +43,7 @@ class LightGCOT(nn.Module):
         self.log_w = nn.Parameter(torch.log(torch.ones(n_potentials) / n_potentials))
         self.a = nn.Parameter(torch.randn(n_potentials, y_dim))
         if A_diagonal_init is not None:
-            self.log_A_diagonal_matrix = nn.Parameter(torch.log(A_diagonal_init * torch.ones(n_potentials, y_dim)))
+            self.A_diagonal_matrix = nn.Parameter(A_diagonal_init * torch.ones(n_potentials, y_dim))
 
     def init_a_by_samples(self, samples):
         assert samples.shape[0] == self.a.shape[0]
@@ -75,126 +75,89 @@ class LightGCOT(nn.Module):
         batch_size = batched_x.shape[0]
         return batched_x.view(batch_size, 1, self.y_dim)  # [bs x M x y_dim]
 
-    def compute_log_B_m(self, batched_x: torch.Tensor) -> torch.Tensor:
+    def compute_B_m(self, batched_x: torch.Tensor) -> torch.Tensor:
         batch_size = batched_x.shape[0]
         # epsilonI = self.epsilon * torch.ones(self.m_potentials // 2, self.y_dim)
         # return torch.cat((epsilonI, epsilonI)).repeat(batch_size, 1, 1)  # [bs x M x y_dim]
-        self.B_m = torch.log(torch.ones(batch_size, 1, self.y_dim))
+        self.B_m = torch.ones(batch_size, 1, self.y_dim)
         return self.B_m  # [bs x M x y_dim]
 
-    def compute_b_nm(self, b_m: torch.Tensor, log_B_m: torch.Tensor) -> tuple[torch.Tensor]:
+    def compute_b_nm(self, b_m: torch.Tensor, B_m: torch.Tensor) -> tuple[torch.Tensor]:
         if self.A_diagonal_init is not None and self.is_B_diagonal:
-            BT_b = torch.exp(log_B_m) * b_m  # [bs x M x y_dim] * [bs x M x y_dim] = [bs x M x y_dim]
+            BT_b = B_m * b_m  # [bs x M x y_dim] * [bs x M x y_dim] = [bs x M x y_dim]
             return 2 * (
-                (torch.exp(self.log_A_diagonal_matrix) * self.a)[None, :, None, :] + BT_b[:, None, :, :]
+                (self.A_diagonal_matrix * self.a)[None, :, None, :] + BT_b[:, None, :, :]
             )  # [1 x N x 1 x y_dim] + [bs x 1 x M x y_dim] = [bs x N x M x y_dim]
         else:
             raise NotImplementedError("Other options are not implemented yet!")
 
-    def compute_c_nm(self, b_m: torch.Tensor, log_B_m: torch.Tensor) -> tuple[torch.Tensor]:
+    def compute_c_nm(self, b_m: torch.Tensor, B_m: torch.Tensor) -> tuple[torch.Tensor]:
         if self.A_diagonal_init is not None and self.is_B_diagonal:
-            bT_B_b = torch.sum(
-                b_m * torch.exp(log_B_m) * b_m, dim=2
-            )  # [bs x M x y_dim] * [bs x M x y_dim] * [bs x M x y_dim]
+            bT_B_b = b_m * B_m * b_m  # [bs x M x y_dim] * [bs x M x y_dim] * [bs x M x y_dim] = [bs x M x y_dim]
             return torch.sum(
-                (self.a * torch.exp(self.log_A_diagonal_matrix) * self.a)[None, :, None, :] + bT_B_b[:, None, :, :],
+                (self.a * self.A_diagonal_matrix * self.a)[None, :, None, :] + bT_B_b[:, None, :, :],
                 dim=3,
             )
             # sum([1 x N x 1 x y_dim] + [bs x 1 x M x y_dim], dim=3) = sum([bs x N x M x y_dim], dim=3) = [bs x N x M]
         else:
             raise NotImplementedError("Other options are not implemented yet!")
 
-    def compute_log_G_nm(self, log_B_m: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+    def compute_G_nm(self, B_m: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
         """
         Computes G_{nm}, G_{nm}^{-1}. TODO: think about memory
         """
         if self.A_diagonal_init is not None and self.is_B_diagonal:
-            log_G = torch.logaddexp(self.log_A_diagonal_matrix[None, :, None, :], log_B_m[:, None, :, :])
+            G = self.A_diagonal_matrix[None, :, None, :] + B_m[:, None, :, :]
             # [1 x N x 1 x y_dim] + [bs x 1 x M x y_dim] = [bs x N x M x y_dim]
-            G_inv = -log_G
-            return log_G, G_inv
+            G_inv = 1 / G
+            return G, G_inv
         else:
             raise NotImplementedError("Other options are not implemented yet!")
 
-    def compute_g_nm(self, b_nm: torch.Tensor, log_G_inv: torch.Tensor) -> tuple[torch.Tensor]:
+    def compute_g_nm(self, b_nm: torch.Tensor, G_inv_nm: torch.Tensor) -> tuple[torch.Tensor]:
         if self.A_diagonal_init is not None and self.is_B_diagonal:
-            return (
-                0.5 * torch.exp(log_G_inv) * b_nm
-            )  # [bs x N x M x y_dim] * [bs x N x M x y_dim] = [bs x N x M x y_dim]
+            return 0.5 * G_inv_nm * b_nm  # [bs x N x M x y_dim] * [bs x N x M x y_dim] = [bs x N x M x y_dim]
         else:
             raise NotImplementedError("Other options are not implemented yet!")
 
     def compute_log_alpha_nm(
-        self, log_v_m: torch.Tensor, log_B_m: torch.Tensor, log_G_nm: torch.Tensor, c_nm: torch.Tensor
+        self, log_v_m: torch.Tensor, B_m: torch.Tensor, G_nm: torch.Tensor, c_nm: torch.Tensor
     ) -> tuple[torch.Tensor]:
         if self.A_diagonal_init is not None and self.is_B_diagonal:
             return (
                 self.log_w[None, :, None]
                 + log_v_m[:, None, :]
-                + 0.5 * (log_B_m.sum(dim=2)[:, None, :] - log_G_nm.sum(dim=3) - c_nm / self.epsilon)
+                + 0.5 * (torch.log(B_m).sum(dim=2)[:, None, :] - torch.log(G_nm).sum(dim=3) - c_nm / self.epsilon)
             )  # [1 x N x 1] + [bs x 1 x M] + [bs x 1 x M] + [bs x N x M] + [bs x N x M] = [bs x N x M]
         else:
             raise NotImplementedError("Other options are not implemented yet!")
 
-    # def compute_log_Z(self, log_alpha_nm: torch.Tensor, G_inv_nm: torch.Tensor, b_nm: torch.Tensor) -> float:
-    #     if self.A_diagonal_init is not None and self.is_B_diagonal:
-    #         return torch.logsumexp(
-    #             log_alpha_nm + 0.125 * torch.sum(b_nm * G_inv_nm * b_nm, dim=3) / self.epsilon,
-    #             dim=(1, 2),
-    #         )  # sum([bs x N x M] + [bs x N x M], dim=(1, 2)) = [bs]
-    #     else:
-    #         raise NotImplementedError("Other options are not implemented yet!")
-
-    def compute_log_Z_nm(self, log_v_m: torch.Tensor, log_B_m: torch.Tensor, b_m: torch.Tensor) -> float:
-        if self.A_diagonal_init is not None and self.is_B_diagonal:
-            log_det_B_m = log_B_m.sum(dim=2)  # [bs x M]
-            log_G_nm = torch.logaddexp(
-                self.log_A_diagonal_matrix[None, :, None, :], log_B_m[:, None, :, :]
-            )  # [1 x N x 1 x y_dim] + [bs x 1 x M x y_dim] = [bs x N x M x y_dim]
-            log_det_G_nm = log_G_nm.sum(dim=3)  # [bs x N x M]
-            log_coeff_nm = torch.logsumexp(
-                self.log_A_diagonal_matrix[None, :, None, :]
-                + log_B_m[:, None, :, :]
-                - log_G_nm
-                + 2 * torch.log(torch.abs(self.a[None, :, None, :] + b_m[:, None, :, :])),
-                dim=3,
-            )  # sum([1 x N x 1 y_dim] + [bs x 1 x M x y_dim] - [bs x N x M x y_dim] + [1 x N x 1 y_dim] + [bs x 1 x M x y_dim], dim=3) = [bs x N x M]
-            return (
-                log_v_m[:, None, :]
-                + self.log_w[None, :, None]
-                + 0.5 * (log_det_B_m[:, None, :] - log_det_G_nm - torch.exp(log_coeff_nm) / self.epsilon)
-            )  # [bs x N x M] + [bs x N x M] = [bs]
-        else:
-            raise NotImplementedError("Other options are not implemented yet!")
-
-    def compute_log_beta_nm(
-        self, g_nm: torch.Tensor, log_G_nm: torch.Tensor, log_alpha_nm: torch.Tensor, log_Z: torch.Tensor
-    ) -> torch.Tensor:
+    def compute_log_Z_nm(self, log_alpha_nm: torch.Tensor, G_inv_nm: torch.Tensor, b_nm: torch.Tensor) -> float:
         if self.A_diagonal_init is not None and self.is_B_diagonal:
             return (
-                log_alpha_nm
-                + 0.5 / self.epsilon * torch.sum(2 * torch.log(torch.abs(g_nm)) + log_G_nm, dim=3)
-                - log_Z[:, None, None]
-            )
-            # [bs x N x M] + [bs x N x M] - [bs x 1 x 1] = [bs x N x M]
+                log_alpha_nm + 0.125 * torch.sum(b_nm * G_inv_nm * b_nm, dim=3) / self.epsilon
+            )  # [bs x N x M] + [bs x N x M] = [bs x N x M]
         else:
             raise NotImplementedError("Other options are not implemented yet!")
 
     def compute_primal_potential(self, batched_y: torch.Tensor) -> float:
         if self.A_diagonal_init is not None and self.is_B_diagonal:
-            log_diff = 2 * torch.log(
-                torch.abs(batched_y[:, None, :] - self.a[None, :, :])
-            )  # [bs x 1 x y_dim] - [1 x N x y_dim] = [bs x N x y_dim]
+            diff = batched_y[:, None, :] - self.a[None, :, :]  # [bs x 1 x y_dim] - [1 x N x y_dim] = [bs x N x y_dim]
             log_quadratic = (
-                -0.5 / self.epsilon * torch.sum(log_diff + self.log_A_diagonal_matrix[None, :, :], dim=2)
+                -0.5 / self.epsilon * torch.sum(diff * self.A_diagonal_matrix[None, :, :] * diff, dim=2)
             )  # [bs x N]
             return self.epsilon * torch.logsumexp(self.log_w[None, :] + log_quadratic, dim=1)  # [bs]
 
     def compute_dual_potential(self, batched_x: torch.Tensor) -> float:
-        log_B_m = self.compute_log_B_m(batched_x)  # [bs x M x y_dim]
+        B_m = self.compute_B_m(batched_x)  # [bs x M x y_dim]
         b_m = self.compute_b_m(batched_x)  # [bs x M x y_dim]
         log_v_m = self.compute_log_v_m(batched_x)  # [bs x M]
-        log_Z_nm = self.compute_log_Z_nm(log_v_m, log_B_m, b_m)
+
+        G_nm, G_inv_nm = self.compute_G_nm(B_m)
+        c_nm = self.compute_c_nm(b_m, B_m)
+        b_nm = self.compute_b_nm(b_m, B_m)
+        log_alpha_nm = self.compute_log_alpha_nm(log_v_m, B_m, G_nm, c_nm)
+        log_Z_nm = self.compute_log_Z_nm(log_alpha_nm, G_inv_nm, b_nm)
         return -self.epsilon * torch.logsumexp(log_Z_nm, dim=(1, 2))  # [bs]
 
     def set_epsilon(self, new_epsilon):
@@ -215,21 +178,23 @@ class LightGCOT(nn.Module):
         for i in range(num_sampling_iterations):
             sub_batch_x = batched_x[sampling_batch_size * i : sampling_batch_size * (i + 1)]
 
-            log_B_m = self.compute_log_B_m(sub_batch_x)  # [bs x M x y_dim]
+            B_m = self.compute_B_m(sub_batch_x)  # [bs x M x y_dim]
             b_m = self.compute_b_m(sub_batch_x)  # [bs x M x y_dim]
             log_v_m = self.compute_log_v_m(sub_batch_x)  # [bs x M]
-            log_Z_nm = self.compute_log_Z_nm(log_v_m, log_B_m, b_m)  # [bs x N x M x y_dim]
 
-            b_nm = self.compute_b_nm(b_m, log_B_m)  # [bs x N x M x y_dim]
+            c_nm = self.compute_c_nm(b_m, B_m)
+            b_nm = self.compute_b_nm(b_m, B_m)  # [bs x N x M x y_dim]
 
-            _, log_G_inv_nm = self.compute_log_G_nm(log_B_m)
-            # [bs x N x M x y_dim], [bs x N x M x y_dim], [bs x N x M x y_dim]
-            g_nm = self.compute_g_nm(b_nm, log_G_inv_nm)  # [bs x N x M x y_dim]
+            G_nm, G_inv_nm = self.compute_G_nm(B_m)  # [bs x N x M x y_dim], [bs x N x M x y_dim]
+            g_nm = self.compute_g_nm(b_nm, G_inv_nm)  # [bs x N x M x y_dim]
 
-            loc = g_nm.view(sampling_batch_size, self.n_potentials * self.m_potentials, self.y_dim)
-            logits = log_Z_nm.view(sampling_batch_size, self.n_potentials * self.m_potentials)
-            scale = torch.sqrt(self.epsilon * torch.exp(log_G_inv_nm)).view(
-                sampling_batch_size, self.n_potentials * self.m_potentials, self.y_dim
+            log_alpha_nm = self.compute_log_alpha_nm(log_v_m, B_m, G_nm, c_nm)
+            log_Z_nm = self.compute_log_Z_nm(log_alpha_nm, G_inv_nm, b_nm)
+
+            loc = g_nm.view(min(sampling_batch_size, batch_size), self.n_potentials * self.m_potentials, self.y_dim)
+            logits = log_Z_nm.view(min(sampling_batch_size, batch_size), self.n_potentials * self.m_potentials)
+            scale = torch.sqrt(self.epsilon * G_inv_nm).view(
+                min(sampling_batch_size, batch_size), self.n_potentials * self.m_potentials, self.y_dim
             )
             if self.A_diagonal_init is not None and self.is_B_diagonal:
                 mix = Categorical(logits=logits)

tests/test_g_and_G.py
@@ -4,27 +4,24 @@ from src.light_gcot import LightGCOT
 
 
 def test_compute_G_nm(D: LightGCOT, B_m: torch.Tensor, batch_size: int):
-    G_nm, G_inv_nm, GGT_nm = D.compute_G_nm(B_m)
+    G_nm, G_inv_nm = D.compute_G_nm(B_m)
 
     _G_nm = torch.zeros((batch_size, D.n_potentials, D.m_potentials, D.y_dim))
     _G_inv_nm = torch.zeros((batch_size, D.n_potentials, D.m_potentials, D.y_dim))
-    _GGT_nm = torch.zeros((batch_size, D.n_potentials, D.m_potentials, D.y_dim))
 
     for n in range(D.n_potentials):
         for m in range(D.m_potentials):
             _G_nm[:, n, m, :] = D.A_diagonal_matrix[n] + B_m[:, m]
             _G_inv_nm[:, n, m, :] = 1 / (D.A_diagonal_matrix[n] + B_m[:, m])
-            _GGT_nm[:, n, m, :] = 1 / (D.A_diagonal_matrix[n] + B_m[:, m] + D.A_diagonal_matrix[n] + B_m[:, m])
 
     assert torch.allclose(G_nm, _G_nm)
     assert torch.allclose(G_inv_nm, _G_inv_nm)
-    assert torch.allclose(GGT_nm, _GGT_nm)
 
 
 def test_compute_g_nm(D: LightGCOT, b_m: torch.Tensor, B_m: torch.Tensor, batch_size: int):
     b_nm = D.compute_b_nm(b_m, B_m)
-    _, _, GGT_inv_nm = D.compute_G_nm(B_m)
-    g_nm = D.compute_g_nm(b_nm, GGT_inv_nm)
+    _, G_inv_nm = D.compute_G_nm(B_m)
+    g_nm = D.compute_g_nm(b_nm, G_inv_nm)
     _g_nm = torch.zeros((batch_size, D.n_potentials, D.m_potentials, D.y_dim))
 
     for n in range(D.n_potentials):